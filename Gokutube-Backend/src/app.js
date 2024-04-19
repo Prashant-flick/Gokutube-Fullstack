6068939afe181415ae997765d1756147a6c1d3f4
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-19 23:38:50 +0530

    update

--- Gokutube-Backend/src/app.js
+++ Gokutube-Backend/src/app.js
@@ -6,12 +6,13 @@ import bodyParser from 'body-parser'
 
 const app = express()
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 
 app.use(express.json({limit: '16kb'}))
 app.use(express.urlencoded({extended: true, limit: '30kb'}))
