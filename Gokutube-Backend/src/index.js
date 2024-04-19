cf7edefb8d275edf553f0d24c6bf2493701c67b1
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-19 17:14:32 +0530

    Update root endpoint response in app.js and index.js

--- Gokutube-Backend/src/index.js
+++ Gokutube-Backend/src/index.js
@@ -14,8 +14,6 @@ connectDB()
         throw err
     })
 
-    app.get("/", (req, res) => res.send("Express on Vercel"));
-
     app.listen(conf.port || 8000, () => {
         console.log("Server is running on port: ", conf.port)
     })
