c114c7ff1332a4f03f80f5c9539a8d1a38dcc31e
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-19 18:57:11 +0530

    Update API base URL and proxy configuration

--- Gokutube-Frontend/vite.config.js
+++ Gokutube-Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://gokutube-fullstack-backend.vercel.app'
+      '/api': 'https://gokutube-fullstack-backend.vercel.app/'
     }
   },
   plugins: [react()],
