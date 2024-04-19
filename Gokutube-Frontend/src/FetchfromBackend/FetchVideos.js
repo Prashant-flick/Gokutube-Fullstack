fb1df078fe18dcb9fb93b0e0c8bad440662a9b97
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-19 23:44:32 +0530

    update imp

--- Gokutube-Frontend/src/FetchfromBackend/FetchVideos.js
+++ Gokutube-Frontend/src/FetchfromBackend/FetchVideos.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from "../api/axios.js"
 
 const FetchAllVidoes = async({
     limit=9,
     query=""
 }) => {
     try {
-        const response = await axios.get(`https://gokutube-fullstack-backend.vercel.app/api/v1/videos/get-all-videos?limit=${limit}&query=${query}`);
+        const response = await axios.get(`/api/v1/videos/get-all-videos?limit=${limit}&query=${query}`);
         return response.data.data
     } catch (error) {
         console.log(error);
