2423234cb0ecefc37c90bea602c09514d68480a0
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-19 18:50:22 +0530

    Update API base URL in axios.js and vite.config.js

--- Gokutube-Frontend/vite.config.js
+++ Gokutube-Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api/': 'http://localhost:8000'
+      '/api': 'https://gokutube-fullstack-backend.vercel.app'
     }
   },
   plugins: [react()],
