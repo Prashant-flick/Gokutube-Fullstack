93e783c0f458cc9198c2501c287e8ae204fc961a
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-20 10:01:55 +0530

    i am tired now

--- Gokutube-Frontend/vite.config.js
+++ Gokutube-Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': String(process.env.API_URL)
+      '/': String(process.env.API_URL)
     }
   },
   plugins: [react()],
