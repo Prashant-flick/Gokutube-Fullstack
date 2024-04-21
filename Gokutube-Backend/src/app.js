5946cbeed763c8e0e359af3a832b7a21271662bc
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-22 00:11:09 +0530

    app update

--- Gokutube-Backend/src/app.js
+++ Gokutube-Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(cors({
     origin: ['https://gokutube-frontend.vercel.app','https://gokutube.vercel.app',conf.corsOrigin],
-    credentials: 'include',
+    credentials: true,
     // withCredentials: true,
     // allowedHeaders: ['Content-Type', 'Authorization'],
     // exposedHeaders: ['Content-Range', 'X-Content-Range']
