918e4844f7bba4d27a7e313d66cc0475c922ea32
Prashant Kumar <prashantkumar1251@gmail.com>
2024-04-22 10:57:49 +0530

    stringify rocks

--- Gokutube-Backend/src/controllers/video.controller.js
+++ Gokutube-Backend/src/controllers/video.controller.js
@@ -290,9 +290,9 @@ const getAVideobyId = asyncHandler( async (req, res) => {
       console.log('isplaying', isplaying);
       const user = await User.findById(req?.user?._id)
       console.log(user);
-      if(JSON.stringify(user?.watchHistory[0])!=JSON.stringify(video[0]?._id)){
-        user?.watchHistory = user?.watchHistory?.filter((id) => id!=new mongoose.Types.ObjectId(video[0]?._id))
-        user?.watchHistory.unshift(new mongoose.Types.ObjectId(video[0]?._id))
+      if(JSON.stringify(user?.watchHistory[0]) !== JSON.stringify(video[0]?._id)){
+        user?.watchHistory = user?.watchHistory?.filter((id) => JSON.stringify(id) !== JSON.stringify(video[0]?._id))
+        user?.watchHistory.unshift(video[0]?._id)
         const watchHistory = user?.watchHistory
         console.log(watchHistory);
 
